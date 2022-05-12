6648af4786cd2e8d91fca538347f1d2031240613
Sergey Kononenko <hackfeed@yandex.ru>
2022-05-12 16:30:22 +0300

    Split source code into logic instances

---
+++ src/train/image.py
@@ -0,0 +1,15 @@
+import cv2
+
+
+def convert_one_channel(img):
+    if len(img.shape) > 2:
+        return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    return img
+
+
+def convert_rgb(img):
+    if len(img.shape) == 2:
+        return cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+
+    return img
